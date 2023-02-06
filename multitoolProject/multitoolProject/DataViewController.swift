beb33fd82d69c89b421f7935a6428bce2fafb288
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2023-02-06 21:21:24 +0100

    setup tabview for split work

---
+++ multitoolProject/multitoolProject/DataViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DataViewController.swift
+//  multitoolProject
+//
+//  Created by Bence Balazs on 06.02.23.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
