f41c8afa5d3a4b2d6c9fd637c2c2d333ea84d87b
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2023-02-07 22:20:28 +0100

    Finished implementation of PDE
    Finished implementation of PhotoDataEditor

--- multitoolProject/multitoolProject/DataViewController.swift
+++ multitoolProject/multitoolProject/DataViewController.swift
@@ -19,6 +19,8 @@ class DataViewController: UIViewController {
     @IBOutlet weak var panoramaStorage: UILabel!
     @IBOutlet weak var videoStorage: UILabel!
     
+    private var commonPhotos = [PHAsset]()
+    
     @IBAction func loadEverything(_ sender: UIButton) {
         commonPhoto.text = "TestText"
         loadAllInformations()
@@ -26,12 +28,10 @@ class DataViewController: UIViewController {
         print("Capacity : \(cap) MiB")
         var freeCap = getFreeStorageCapacity()
         print("Free capacity : \(freeCap) MiB")
-
-
     }
     
     @IBAction func deleteEverything(_ sender: UIButton) {
-        deleteAllImages()
+        deleteAllImages(commonPhotos)
     }
     
     override func viewDidLoad() {
@@ -42,69 +42,121 @@ class DataViewController: UIViewController {
     
     func loadAllInformations(){
         //Image Data
+        
         let fetchOptionsImage = PHFetchOptions()
         fetchOptionsImage.sortDescriptors = [NSSortDescriptor(key:"creationDate",ascending:false)]
         let fetchResultImage = PHAsset.fetchAssets(with: .image, options: fetchOptionsImage)
-        
-        //Debug Data
         /*
+        //Debug Data
         let fetchOptions = PHFetchOptions()
-        fetchOptions.predicate = NSPredicate(format: "mediaType == %d", PHAssetMediaType.image.rawValue)
+        fetchOptions.predicate = NSPredicate(format: "mediaType == %d AND mediaSubtype != %d", PHAssetMediaType.image.rawValue, PHAssetMediaSubtype.photoPanorama.rawValue)
         let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
          */
-        
-        //Panorama Data
-        let panoOptions = PHFetchOptions()
-        panoOptions.predicate = NSPredicate(format: "mediaSubtype == %d", PHAssetMediaSubtype.photoPanorama.rawValue)
-        let fetchResultPano = PHAsset.fetchAssets(with: .image, options: panoOptions)
         
         //LivePhoto Data
         let livePhotoOptions = PHFetchOptions()
         livePhotoOptions.predicate = NSPredicate(format: "mediaSubtype == %d", PHAssetMediaSubtype.photoLive.rawValue)
         let fetchResultLivePhoto = PHAsset.fetchAssets(with: .image, options: livePhotoOptions)
         
+        //Panorama Data
+        let panoOptions = PHFetchOptions()
+        panoOptions.predicate = NSPredicate(format: "mediaSubtype == %d", PHAssetMediaSubtype.photoPanorama.rawValue)
+        let fetchResultPano = PHAsset.fetchAssets(with: .image, options: panoOptions)
+
         //Video Data
         let fetchOptionsVideo = PHFetchOptions()
         fetchOptionsVideo.predicate = NSPredicate(format: "mediaType == %d", PHAssetMediaType.video.rawValue)
         let fetchResultVideo = PHAsset.fetchAssets(with: .video, options: fetchOptionsVideo)
         
-        /*
-        print("Debug \(fetchResult.count)")
-        iterateOverMediatype(fetchResult)
-        */
-         
-        print("Image \(fetchResultImage.count)")
-        iterateOverMediatype(fetchResultImage)
-        commonPhoto.text = "Normal images: \(fetchResultImage.count)"
+        //Images without Live and Panorama
+        commonPhotos = getCommonImages(fetchResultImage, fetchResultLivePhoto, fetchResultPano)
+        print("Image \(commonPhotos.count)")
+        let memoryUsageCommonPhoto = round(iterateOverMediatypePHAssets(commonPhotos)*10)/10
+        commonPhotoStorage.text = "\(memoryUsageCommonPhoto) in MiB"
+        commonPhoto.text = "Normal images: \(commonPhotos.count)"
         
-        print("Pano \(fetchResultPano.count)")
-        iterateOverMediatype(fetchResultPano)
-        panorama.text = "Panorama images: \(fetchResultPano.count)"
-
         print("LivePhoto \(fetchResultLivePhoto.count)")
-        iterateOverMediatype(fetchResultLivePhoto)
+        let memoryUsageLivePhoto = round(iterateOverMediatype(fetchResultLivePhoto)*10)/10
+        livePhotoStorage.text = "\(memoryUsageLivePhoto) in MiB"
         livePhoto.text = "Live images: \(fetchResultLivePhoto.count)"
 
+        print("Pano \(fetchResultPano.count)")
+        let memoryUsagePanorama = round(iterateOverMediatype(fetchResultPano)*10)/10
+        panoramaStorage.text = "\(memoryUsagePanorama) in MiB"
+        panorama.text = "Panorama images: \(fetchResultPano.count)"
+        
         print("Video \(fetchResultVideo.count)")
-        getMemoryUsageVideo(fetchResultVideo)
+        let memoryUsageVideo = round(getMemoryUsageVideo(fetchResultVideo)*10)/10
+        videoStorage.text = "\(memoryUsageVideo) in MiB"
         video.text = "Videos: \(fetchResultVideo.count)"
-
     }
     
-    func iterateOverMediatype(_ fetchResult: PHFetchResult<PHAsset>){
+    func iterateOverMediatype(_ fetchResult: PHFetchResult<PHAsset>) -> Double{
         if(fetchResult.count > 0){
+            var totalMemory = 0.0
+            
             for i in 0..<fetchResult.count{
                 let asset = fetchResult.object(at: i)
                 let memoryUsage = getMemoryUsage(for: asset)
                 //Byte to MiB
                 let fileSize = (Double(memoryUsage) / 1048576.0)
+                totalMemory += fileSize
                 print(fileSize)
                 print(asset.value(forKey: "filename") as? String ?? "Nil")
+                print(asset.localIdentifier)
             }
-            
+            return totalMemory
         }else{
             print("No photos available")
+            return 0.0
         }
+    }
+    
+    func iterateOverMediatypePHAssets(_ fetchResult: [PHAsset]) -> Double{
+        if(fetchResult.count > 0){
+            var totalMemory = 0.0
+            
+            for i in 0..<fetchResult.count{
+                let asset = fetchResult[i]
+                let memoryUsage = getMemoryUsage(for: asset)
+                //Byte to MiB
+                let fileSize = (Double(memoryUsage) / 1048576.0)
+                totalMemory += fileSize
+                print(fileSize)
+                print(asset.value(forKey: "filename") as? String ?? "Nil")
+                print(asset.localIdentifier)
+            }
+            return totalMemory
+        }else{
+            print("No photos available")
+            return 0.0
+        }
+    }
+    
+    func getCommonImages(_ commonImageResult: PHFetchResult<PHAsset>,_ livePhotoResult: PHFetchResult<PHAsset>,_ panoramaResult: PHFetchResult<PHAsset>) -> [PHAsset]{
+        
+        var onlyImages: [PHAsset] = [PHAsset]()
+        
+        for i in 0..<commonImageResult.count{
+            let x = commonImageResult.object(at: i)
+            var isUnique = true;
+            for j in 0..<livePhotoResult.count{
+                let live = livePhotoResult.object(at: j)
+                if(x.localIdentifier == live.localIdentifier){
+                    isUnique = false
+                }
+            }
+            for j in 0..<panoramaResult.count{
+                let pano = panoramaResult.object(at: j)
+                if(x.localIdentifier == pano.localIdentifier){
+                    isUnique = false
+                }
+            }
+            if(isUnique){
+                onlyImages.append(x)
+            }
+        }
+        return onlyImages
     }
     
     func getMemoryUsage(for asset: PHAsset) -> Int {
@@ -119,8 +171,10 @@ class DataViewController: UIViewController {
         return imageData?.count ?? 0
     }
     
-    func getMemoryUsageVideo(_ fetchResult: PHFetchResult<PHAsset>){
+    func getMemoryUsageVideo(_ fetchResult: PHFetchResult<PHAsset>) -> Double{
         if fetchResult.count > 0 {
+            var totalMemory = 0.0
+            
             for i in 0..<fetchResult.count {
                 let asset = fetchResult.object(at:i)
                 let resource = PHAssetResource.assetResources(for: asset).first
@@ -128,26 +182,34 @@ class DataViewController: UIViewController {
                 //File in MiB
                 let fileSizeInMB = Double(fileSize ?? 0) / (1024.0 * 1024.0)
                 print("Video file size: \(fileSizeInMB) MiB")
+                totalMemory += fileSizeInMB
             }
+            return totalMemory
         } else {
-            print("no videos found")
+            print("No videos found")
+            return 0.0
         }
     }
     
-    func deleteAllImages(){
+    func deleteAllImages(_ assetsToDelete: [PHAsset]){
+        /*
         let fetchOptions = PHFetchOptions()
         let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-
+        
         var assetsToDelete = [PHAsset]()
         fetchResult.enumerateObjects { (asset, _, _) in
             assetsToDelete.append(asset)
         }
+         */
         
         PHPhotoLibrary.shared().performChanges({
             PHAssetChangeRequest.deleteAssets(assetsToDelete as NSArray)
         }, completionHandler: {success, error in
             if success {
                 print("All photos deleted successfully")
+                DispatchQueue.main.sync {
+                    self.loadAllInformations()
+                }
             } else {
                 print("Error deleting photos")
             }
